3738f8d0576ff5a17ac301e90c5271d3cc0c6378
danielcy <448219947@qq.com>
2016-11-21 18:58:03 +0800

    Commit the whole tutorial game.

---
+++ RollABall/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public float speed;
+	public Text countText;
+	public Text winText;
+
+	private Rigidbody rigidbody;
+	private int count;
+
+	void Start () {
+		rigidbody = GetComponent< Rigidbody >();
+		count = 0;
+		setCountText();
+		winText.text = "";
+	}
+
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rigidbody.AddForce (movement * speed);
+
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag("Pick Up")) {
+			other.gameObject.SetActive(false);
+			count = count + 1;
+			setCountText ();
+			setWinText ();
+		}
+	}
+
+	void setCountText () {
+		countText.text = "Count: " + count.ToString();
+	}
+	void setWinText() {
+		if (count == 10) {
+			winText.text = "You Win!";
+		}
+	}
+}
